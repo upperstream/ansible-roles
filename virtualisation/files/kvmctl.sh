5564757aa9b2186c89e57088fc5725ef830d242c
upperstream <upperstream@users.noreply.github.com>
2024-07-31 22:39:23 +0900

    virtualisation: `kvmctl.sh` script supports AMD processors in addition to Intel's

--- virtualisation/files/kvmctl.sh
+++ virtualisation/files/kvmctl.sh
@@ -7,21 +7,39 @@ usage() {
 
 	enable:
 	  Enable KVM.
-
 	disable:
 	  Disale KVM.
-
 	status:
 	  Print whether KVM is enabled or not.  Exit with the status 1 if disabled.
-
 	check:
 	  Print whether KVM is supported on this host or not.  Exit with the status 1 if unsupported.
-
 	help:
 	  Print this usage guide and exit.  This is the default behaviour if no command is specified.
 	EOF
 }
 
+# Regex pattern for KVM kernel modules
+pattern=
+if grep -cE "`printf '^vendor_id[ \t:]+GenuineIntel$'`" /proc/cpuinfo > /dev/null; then
+	pattern="$pattern|-intel"
+fi
+if grep -cE "`printf '^vendor_id[ \t:]+AuthenticAMD$'`" /proc/cpuinfo > /dev/null; then
+	pattern="$pattern|-amd"
+fi
+pattern="kvm($pattern)"
+
+# tac command or alternatives to print stdin lines in reverse order
+_tac() { tac "$@"; }
+if [ -f /etc/os-release ]; then
+	. /etc/os-release
+fi
+if echo "$NAME" | grep 'BSD$' > /dev/null && `command -v tail > /dev/null`; then
+	_tac() { tail -r "$@"; }
+elif ! `command -v tac > /dev/null`; then
+	_tac() { awk '{ line[i++] = $0} END { for (j = i - 1; j >= 0; j--) print line[j]; }' "$@"; }
+fi
+
+# sudo command or alternatives to escalate user privilege
 for s in doas sudo; do
 	if type $s 2>&1 >/dev/null; then
 		_sudo=$s
@@ -30,17 +48,17 @@ done
 
 case "$1" in
 	enable)
-		for m in $(locate kvm | grep -E '/kvm(|-intel).ko' | grep "$(uname -r)"); do
+		for m in `locate kvm | grep -E "/$pattern\.ko" | grep "\`uname -r\`" | _tac`; do
 			$_sudo insmod "$m"
 		done
 		;;
 	disable)
-		for m in $(lsmod | awk '($1 ~ /kvm/) {print $1}'); do
+		for m in `lsmod | awk '($1 ~ /kvm/) {print $1}'`; do
 			$_sudo rmmod "$m"
 		done
 		;;
 	status)
-		if lsmod | grep -E 'kvm(|-intel)' > /dev/null; then
+		if lsmod | grep -E "$pattern" > /dev/null; then
 			echo "KVM is enabled"
 			exit 0
 		else
@@ -49,7 +67,7 @@ case "$1" in
 		fi
 		;;
 	check)
-		if [ "`grep -cE '(vmx|svm)' /proc/cpuinfo`" -gt 0 ]; then
+		if grep -cE '(vmx|svm)' /proc/cpuinfo > /dev/null; then
 			echo "KVM is supported"
 			exit 0
 		else
